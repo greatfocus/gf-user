26929f4d583b86b041d5971f48d1ea20362bd422
johnmuthurimi <mucunga90@gmail.com>
2020-11-14 14:57:19 +0300

    pass env variable via arguments

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	frame "github.com/greatfocus/gf-frame"
 	"github.com/greatfocus/gf-user/router"
 	_ "github.com/lib/pq"
@@ -8,8 +10,13 @@ import (
 
 // Entry point to the solution
 func main() {
+	// Get arguments
+	if os.Args[1] == "" {
+		panic("Pass the environment")
+	}
+
 	// Load configurations
-	server := frame.Create("dev.json")
+	server := frame.Create(os.Args[1] + ".json")
 	server.Cron.Start()
 
 	// start API service
