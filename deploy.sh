f3d3a405cba980033fbd393f80648a52855f4691
johnmuthurimi <mucunga90@gmail.com>
2020-11-14 15:23:53 +0300

    protect config file

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,10 @@ export PATH=$PATH:/usr/local/go/bin
 # Building our app
 GOOS=linux GOARCH=amd64 go build
 
+# project config
+sudo chmod -R 700 dev.json
+sudo chown -R muthurimi dev.json
+
 # create service
 sudo systemctl stop gf-user 
 sudo systemctl disable gf-user  
